927ef93a571ec5577a04d4dc7924e29ac4f36e13
VISHANT PANDIT <vishantpandit80@gmail.com>
2026-03-08 17:29:56 +0530

    Update Random.py

--- Random.py
+++ Random.py
@@ -1,14 +1,15 @@
 import random
 
-# ----------fot random digit----------
+# ----------fot random digit----
 print(random.randrange(1,1000))
 print(x)
 
-----------for random no. in given data---------
+----------for random no. in given data-----
 print(random.choice(['A','B','C','D','E']))  
 
-# ----------for multiple choice----------
+# ----------for multiple choice-----
 print(random.choice(['red','green','yellow']))
 
-# -----------for multiple sample----------
+# -----------for multiple sample----
 print(random.sample(['1','2','3','4','5','6'],3))
+
